ff55dbe5dfa7011b9b7ef852725373b73524af69
Carlos <charles.sl@icloud.com>
2025-03-20 21:15:20 -0600

    animation to open new screen

--- sweetclick/lib/screens/initial_screen.dart
+++ sweetclick/lib/screens/initial_screen.dart
@@ -158,8 +158,28 @@ class InitialScreen extends StatefulWidget {
                 leading: Icon(Icons.control_point, color: Colors.pink),
                 title: Text('Manage desserts'),
                 onTap: () {
-                   Navigator.push(context,MaterialPageRoute(builder: (_)=>ManageDesserts()));
-                   
+                  
+                    Navigator.of(context).push(
+                      //animation
+               PageRouteBuilder(
+            
+                  pageBuilder: (context, animation, secondaryAnimation) =>  ManageDesserts(),
+                  transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                    const begin = Offset(0.0,1.0);
+                    const end = Offset.zero;
+                    const curve = Curves.ease;
+                    var tween = Tween(
+                      begin: begin, end: end).chain(CurveTween(curve: curve)
+                      );
+                      var offsetAnimation = animation.drive(tween);
+
+                      return SlideTransition(
+                        position: offsetAnimation,
+                        child: ManageDesserts(),
+                        );
+                  } ,
+                ),
+                );
                 
                
                 },
